b0979a1a7f8ac94048b5cd2e9b8ebe2bc3832dfc
Charanrakesh <charanrakesh3555@gmail.com>
2024-07-16 23:02:26 -0700

    a webpage is add to the repository

--- git.py
+++ git.py
@@ -1,1 +1,8 @@
-print("hello world")
+print("hello")
+input = input("Say hi here:")
+if "hi" in input:
+    print("you are a good guy")
+    print("i love you")
+else:
+    print("Your are Bad guy")
+    print("I Hate you")
