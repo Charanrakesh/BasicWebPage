b767dbb938d50ec5b718a5875c7054553e98b366
Charanrakesh <charanrakesh3555@gmail.com>
2024-07-17 15:29:00 +0530

    This is the first best website

--- git.py
+++
@@ -1,8 +0,0 @@
-print("hello")
-input = input("Say hi here:")
-if "hi" in input:
-    print("you are a good guy")
-    print("i love you")
-else:
-    print("Your are Bad guy")
-    print("I Hate you")
---
+++ home.js
@@ -0,0 +1,4 @@
+function call()
+{
+    alert("Your are on the home page")
+}
